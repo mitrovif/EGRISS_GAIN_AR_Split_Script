44697048fa38c098324f75275ca035b58c77fc25
Ladina <125482516+l-sara@users.noreply.github.com>
2025-05-05 14:44:36 -0400

    Update AR.1.5 Overall.R
    adjust table further, rename table

--- AR.1.5 Overall.R
+++ AR.1.5 Overall.R
@@ -18,7 +18,7 @@ list_countries_by_region <- function(group_roster) {
 }
 
 # List of Countries by Region
-country_list_flextable <- flextable(list_countries_by_region(group_roster)) %>%
+ar.1.5 <- flextable(list_countries_by_region(group_roster)) %>%
   delete_columns(j = "ryear") %>%
   set_table_properties(width = 0.5, layout = "autofit") %>%
   theme_booktabs() %>%
@@ -26,16 +26,16 @@ country_list_flextable <- flextable(list_countries_by_region(group_roster)) %>%
   bg(part = "header", bg = primary_color) %>%
   color(part = "header", color = "black") %>%
   border_outer(border = fp_border(color = "black", width = 2)) %>%
-  border_inner(border = fp_border(color = "gray", width = 0.5)) %>%
+  border_inner_h(border = fp_border(color = "gray", width = 0.5)) %>%
+  fontsize(size = 10, part = "header") %>%
+  fontsize(size = 10, part = "body") %>%
   
   # Add footer row with the footnote text (after deleting the column)
   add_footer_row(values = "Footnote: This table presents the list of countries for each region based on metadata information.",
                  colwidths = ncol(list_countries_by_region(group_roster)) - 1) %>%  # Adjust colwidths due to the column deletion
   fontsize(size = 7, part = "footer") %>%
+  
   set_caption("List of Countries by Region")
 
 # Display Second Table
-country_list_flextable
-
-
-
+ar.1.5
